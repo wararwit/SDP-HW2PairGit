ace2253650ad1fbba37d50cda473a4e2c64da0fe
Nutchanon <in.nutchanon_st@tni.ac.th>
2017-01-20 02:56:05 +0000

    First commit

--- __init__.py
+++ __init__.py
@@ -1,6 +1,22 @@
-print("hello")
+print("Random number is")
 import random
 
-random_number=random.randrange(0, 1000000, 1)
+random_number=random.randrange(0, 100000, 1)
 
-print random_number
+
+z=random_number
+
+bit1=z%10;
+bit2=z%10/5;
+bit3=z%100/10;
+bit4=z%1000/100;
+bit5=z%10000/1000;
+bit6=z%100000/10000;
+bit7=9;
+
+
+
+print bit6,bit5,bit4,bit3,bit2,bit1,bit7
+
+print("fix bit1 = 9")
+
